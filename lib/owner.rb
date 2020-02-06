ad2d66f81c0f304eac2a6023e07ba2a2f965b18f
Alehandro Persaud <andrew041994@gmail.com>
2020-02-06 06:56:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -69,13 +69,13 @@ end
   def sell_pets
     dogs.each do |i|
       if i.mood != "nervous"
-        i.mood = "nervous"
+        i.mood = "nervous" && i.owner = nil
       end
     end
 
     cats.each do |i|
       if i.mood != "nervous"
-        i.mood = "nervous"
+        i.mood = "nervous" && i.owner = nil
       end
     end
   end
