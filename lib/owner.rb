e1e2cb6abeb11499b009f90b9f474e6499d66156
Alehandro Persaud <andrew041994@gmail.com>
2020-02-06 06:50:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -67,8 +67,8 @@ end
   end
 
   def sell_pets
-    dogs.each {|i| i.mood = "nervous" && owner = nil}
-    cats.each {|i|i.mood = "nervous" && owner = nil}
+    dogs.each {|i|puts i.mood}
+    # cats.each {|i|i.mood = "nervous" && owner = nil}
   end
 
 
